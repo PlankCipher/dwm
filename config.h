712ec7fffba25aebd5979e418eebf87ad7eb904f
PlankCipher <plankcipher1@protonmail.com>
2021-07-05 07:53:02 -0700

    Apply push patch

--- config.h
+++ config.h
@@ -101,6 +101,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
+	{ MODKEY|ShiftMask,             XK_k,      pushup,         {0} },
+	{ MODKEY|ShiftMask,             XK_j,      pushdown,       {0} },
 };
 
 static Button buttons[] = {
