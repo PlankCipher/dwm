f8c5b6bca6d905a831526af5bd328d9df6986ad9
PlankCipher <plankcipher1@protonmail.com>
2022-06-06 09:04:12 +0200

    Add a keybinding to launch emacs

--- config.h
+++ config.h
@@ -158,6 +158,7 @@ static Key keys[] = {
 	{ APPS_MOD,                     XK_t,                      spawn,          SHCMD("st -e sh -c kabmat") },
 	{ APPS_MOD,                     XK_w,                      spawn,          SHCMD("st -e nmtui") },
 	{ APPS_MOD,                     XK_z,                      spawn,          SHCMD("$HOME/chamber_of_magic/boomer/boomer") },
+	{ APPS_MOD,                     XK_d,                      spawn,          SHCMD("emacs") },
 
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
